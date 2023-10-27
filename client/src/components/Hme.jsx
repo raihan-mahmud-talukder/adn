864b177ad7364207884e8117dbe153b95995245d
raihan-mahmud-talukder <raihan_uits@outlook.com>
2023-10-28 01:03:57 +0600

    Home.jsx

---
+++ client/src/components/Hme.jsx
@@ -0,0 +1,11 @@
+import { Body } from "./Body"
+import { Logo } from "./Logo"
+
+export const Home = () => {
+    return (
+        <div className="">
+            <Logo />
+            <Body />
+        </div>
+    )
+}
